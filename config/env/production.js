98a7b2c662584e0bb0c1b8e1991cfc0ba0c1d87f
unknown <mmallis87@gmail.com>
2015-10-18 22:59:06 -0400

    make it better

--- config/env/production.js
+++ config/env/production.js
@@ -1,39 +1,21 @@
 'use strict';
 
 module.exports = {
-  //db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/mean-prod',
+  //db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
   db: 'mongodb://mmallis87:mongolab@ds037234.mongolab.com:37234/heroku_99cvt30k',
-  /**
-   * Database options that will be passed directly to mongoose.connect
-   * Below are some examples.
-   * See http://mongodb.github.io/node-mongodb-native/driver-articles/mongoclient.html#mongoclient-connect-options
-   * and http://mongoosejs.com/docs/connections.html for more information
-   */
-  dbOptions: {
-    /*
-    server: {
-        socketOptions: {
-            keepAlive: 1
-        },
-        poolSize: 5
-    },
-    replset: {
-      rs_name: 'myReplicaSet',
-      poolSize: 5
-    },
-    db: {
-      w: 1,
-      numberOfRetries: 2
-    }
-    */
+  debug: true,
+  logging: {
+    format: 'tiny'
   },
-  //hostname: 'http://localhost:3000',
+  //  aggregate: 'whatever that is not false, because boolean false value turns aggregation off', //false
+  aggregate: false,
+  mongoose: {
+    debug: false
+  },
+  //hostname: 'https://seefte-mmallis87.c9.io',
   hostname: process.env.IP + process.env.PORT,
   app: {
-    name: 'MEAN - A Modern Stack - Production'
-  },
-  logging: {
-    format: 'combined'
+    name: 'Osender - A fast worldwide mail delivery service.'
   },
   strategies: {
     local: {
@@ -41,41 +23,49 @@ module.exports = {
     },
     landingPage: '/',
     facebook: {
-      clientID: 'APP_ID',
+      clientID: 'DEFAULT_APP_ID',
       clientSecret: 'APP_SECRET',
       callbackURL: 'http://localhost:3000/api/auth/facebook/callback',
       enabled: false
     },
     twitter: {
-      clientID: 'CONSUMER_KEY',
+      clientID: 'DEFAULT_CONSUMER_KEY',
       clientSecret: 'CONSUMER_SECRET',
       callbackURL: 'http://localhost:3000/api/auth/twitter/callback',
       enabled: false
     },
     github: {
-      clientID: 'APP_ID',
+      clientID: 'DEFAULT_APP_ID',
       clientSecret: 'APP_SECRET',
       callbackURL: 'http://localhost:3000/api/auth/github/callback',
       enabled: false
     },
     google: {
-      clientID: 'APP_ID',
-      clientSecret: 'APP_SECRET',
-      callbackURL: 'http://localhost:3000/api/auth/google/callback',
-      enabled: false
+    //  clientID: '542491906202-dj88r52iiclamqrth5umvs36ilvr3d29.apps.googleusercontent.com',
+    //  clientSecret: '-B0a3pEByC-lS6c2SU1OdKM5',
+    //  callbackURL: 'http://localhost:3000/api/auth/google/callback',
+    //  enabled: true
+    //},
+    clientID: "542491906202-dj88r52iiclamqrth5umvs36ilvr3d29.apps.googleusercontent.com",
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri:"https://accounts.google.com/o/oauth2/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    clientSecret: "-B0a3pEByC-lS6c2SU1OdKM5",
+    callbackURL: "http://localhost:3000/api/auth/google/callback",
+    enabled: true
     },
     linkedin: {
-      clientID: 'API_KEY',
-      clientSecret: 'SECRET_KEY',
+      clientID: 'DEFAULT_API_KEY',
+      clientSecret: 'APP_SECRET',
       callbackURL: 'http://localhost:3000/api/auth/linkedin/callback',
       enabled: false
     }
   },
-  emailFrom: 'SENDER EMAIL ADDRESS', // sender address like ABC <abc@example.com>
+  emailFrom: 'mmallis87@gmail.com', // sender address like ABC <abc@example.com>
   mailer: {
-    service: 'SERVICE_PROVIDER',
+    service: 'Gmail', // Gmail, SMTP
     auth: {
-      user: 'EMAIL_ID',
+      user: 'mmallis87@gmail.com',
       pass: 'PASSWORD'
     }
   },
